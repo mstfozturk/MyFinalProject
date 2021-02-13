9c684c52a72e377b7adf9fe74f949bcaa2c2932d
Mustafa ÖZTÜRK <mstfozturk@icloud.com>
2021-02-13 17:05:30 +0300

    Core layer added

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,9 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //Generic constraint -- generic kısıt
     //T : class  ==> referans tip
